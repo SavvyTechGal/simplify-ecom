b9dba2e466b897fdfbd8d720d90410cb7587dfd2
SavvyTechGal <33135612+SavvyTechGal@users.noreply.github.com>
2023-08-08 21:05:00 -0400

    fixed issues

--- insert.sql
+++ insert.sql
@@ -2,9 +2,13 @@ INSERT INTO Business ( Name,  URL)
 SELECT Business_Name, Business_URL
 FROM raw_table;
 
+
+
 INSERT INTO Subscription_Type 
 (Name, Order_Max, Monthly_Price, Yearly_Price)
 SELECT Subscription_Name, Subscription_Order_Max, Subscription_Monthly_Price, Subscription_Yearly_Price FROM raw_table;
+
+
 
 INSERT INTO Subscription (Business_ID, Subscription_Type_ID, Start_Date, End_Date)
 SELECT
