0693dbb25e4f1e515f9ea35a07c8a6b97161144e
DavidA123777 <ddda4167@gmail.com>
2023-08-08 20:59:31 -0400

    added insert.sql

--- insert.sql
+++ insert.sql
@@ -4,4 +4,15 @@ FROM raw_table;
 
 INSERT INTO Subscription_Type 
 (Name, Order_Max, Monthly_Price, Yearly_Price)
-SELECT Subscription_Name, Subscription_Order_Max, Subscription_Monthly_Price, Subscription_Yearly_Price FROM attribute_table;
+SELECT Subscription_Name, Subscription_Order_Max, Subscription_Monthly_Price, Subscription_Yearly_Price FROM raw_table;
+
+INSERT INTO Subscription (Business_ID, Subscription_Type_ID, Start_Date, End_Date)
+SELECT
+    b.Business_ID,
+    st.Subscription_Type_ID,
+    raw.Subscription_Start_Date,
+    raw.Subscription_End_Date
+FROM
+    raw_table raw
+    JOIN Business b ON raw.Business_Name = b.Name
+    JOIN Subscription_Type st ON raw.Subscription_Name = st.Name;
