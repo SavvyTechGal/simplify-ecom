dbda2cad74b8cee93ba6cc5cd8e0786ba3b390d9
SavvyTechGal <33135612+SavvyTechGal@users.noreply.github.com>
2023-08-08 20:12:55 -0400

    create tables

--- project_sql.py
+++ project_sql.py
@@ -96,13 +96,88 @@ try:
 ########################################
 
 ########################################
-    ## Load data from a CSV file into a raq table
-    load_query = """
-    COPY raw_table FROM '/Users/savana/Desktop/Clean Desktop/school/project/dummy_data.csv' WITH CSV HEADER;
-    """
-    cursor.execute(load_query)
-    print("CSV data loaded successfully.")
+    # ## Load data from a CSV file into a raq table
+    # load_query = """
+    # COPY raw_table FROM '/Users/savana/Desktop/Clean Desktop/school/project/dummy_data.csv' WITH CSV HEADER;
+    # """
+    # cursor.execute(load_query)
+    # print("CSV data loaded successfully.")
 
+########################################
+
+
+########################################
+# Create Raw Table
+    create_table_query = """
+CREATE TABLE Business (
+    Business_ID INT PRIMARY KEY,
+    Name VARCHAR(32),
+    URL VARCHAR(32)
+);
+
+    """ 
+    cursor.execute(create_table_query)
+    print("Table created successfully.")
+
+    create_table_query = """
+-- Creating Subscription_Type table
+CREATE TABLE Subscription_Type (
+    Subscription_Type_ID INT PRIMARY KEY,
+    Name VARCHAR(32),
+    Order_Max INT,
+    Monthly_Price INT,
+    Yearly_Price INT
+);
+
+    """ 
+    cursor.execute(create_table_query)
+    print("Table created successfully.")
+
+    create_table_query = """
+-- Creating Subscription table
+CREATE TABLE Subscription (
+    Subscription_ID INT PRIMARY KEY,
+    Business_ID INT,
+    Subscription_Type_ID INT,
+    Start_Date TIMESTAMP,
+    End_Date TIMESTAMP,
+    FOREIGN KEY (Business_ID) REFERENCES Business(Business_ID),
+    FOREIGN KEY (Subscription_Type_ID) REFERENCES Subscription_Type(Subscription_Type_ID)
+);
+
+    """ 
+    cursor.execute(create_table_query)
+    print("Table created successfully.")
+
+
+    create_table_query = """
+-- Creating Customer_Profile table
+CREATE TABLE Customer_Profile (
+    Customer_ID INT PRIMARY KEY,
+    Name VARCHAR(32),
+    Email VARCHAR(32),
+    Phone VARCHAR(32),
+    Created_At TIMESTAMP,
+    Updated_At TIMESTAMP
+);
+
+    """ 
+    cursor.execute(create_table_query)
+    print("Table created successfully.")
+
+    create_table_query = """
+-- Creating Business_Customers linker table
+CREATE TABLE Business_Customers (
+    Business_ID INT,
+    Customer_ID INT,
+    PRIMARY KEY (Business_ID, Customer_ID),
+    FOREIGN KEY (Business_ID) REFERENCES Business(Business_ID),
+    FOREIGN KEY (Customer_ID) REFERENCES Customer_Profile(Customer_ID)
+);
+
+    """ 
+    cursor.execute(create_table_query)
+    print("Table created successfully.")
 ########################################
 
     # Commit the changes
