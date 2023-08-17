15012cb7d74a2dbc30f0d5389d5ed0b743c92b34
SavvyTechGal <33135612+SavvyTechGal@users.noreply.github.com>
2023-08-16 21:12:47 -0400

    final_ddl

--- final/final_dml.sql
+++ final/final_dml.sql
@@ -1,12 +1,3 @@
-INSERT INTO Transaction (dt_of_tran, hr_of_trans, customer_id)
-SELECT
-    dt_of_tran,
-    hr_of_trans,
-    c.customer_id
-FROM raw_table r
-JOIN Customer c ON r.customer = c.c_name;
-
-
 INSERT INTO Customer (c_name, c_age, c_gender, c_cell)
 SELECT DISTINCT
     customer,
@@ -14,14 +5,6 @@ SELECT DISTINCT
     c_gender,
     c_cell
 FROM raw_table;
-
-INSERT INTO Product_Class (product_id, class_type)
-SELECT DISTINCT
-    p.product_id,
-    r.prod_class
-FROM raw_table r
-JOIN Product p ON r.prod_name = p.prod_name;
-
 
 INSERT INTO Product (prod_name, prod_unit_price, prod_weight, prod_weight_unit, manufacturer, supplier)
 SELECT DISTINCT
@@ -33,6 +16,20 @@ SELECT DISTINCT
     r.supplier
 FROM raw_table r;
 
+INSERT INTO Product_Class (product_id, class_type)
+SELECT DISTINCT
+    p.product_id,
+    r.prod_class
+FROM raw_table r
+JOIN Product p ON r.prod_name = p.prod_name;
+
+INSERT INTO Transaction (dt_of_tran, hr_of_trans, customer_id)
+SELECT
+    dt_of_tran,
+    hr_of_trans,
+    c.customer_id
+FROM raw_table r
+JOIN Customer c ON r.customer = c.c_name;
 
 INSERT INTO Transaction_Products (transaction_id, product_id, qty_purchased)
 SELECT
@@ -43,5 +40,16 @@ FROM raw_table r
 JOIN Transaction t ON r.dt_of_tran = t.dt_of_tran AND r.hr_of_trans = t.hr_of_trans 
 JOIN Customer c ON r.customer = c.c_name and c.customer_id = t.customer_id
 JOIN Product p ON r.prod_name = p.prod_name;
+
+
+
+
+
+
+
+
+
+
+
 
 
