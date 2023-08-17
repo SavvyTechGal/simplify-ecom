cfdf1015f2618703b19aee01d47414b0249b7645
SavvyTechGal <33135612+SavvyTechGal@users.noreply.github.com>
2023-08-16 20:42:50 -0400

    table statements

---
+++ final/final_ddl.sql
@@ -0,0 +1,19 @@
+CREATE TABLE raw_table (
+    dt_of_tran INT,
+    hr_of_trans INT,
+    customer VARCHAR(32),
+    c_age VARCHAR(4),
+    c_gender VARCHAR(6),
+    c_cell VARCHAR(32),
+    prod_name VARCHAR(32),
+    generic_or_brand VARCHAR(32),
+    manufacturer VARCHAR(32),
+    supplier VARCHAR(32),
+    prod_class VARCHAR(32),
+    prod_unit_price DOUBLE PRECISION,
+    qty_purchased INT,
+    prod_size VARCHAR(32)
+);
+
+
+
